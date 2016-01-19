aad48af75aa46851c0377e932915abd77c2ac262
KevinSC <kevineitor22@gmail.com>
2016-01-18 21:14:17 -0500

    se realizo todo lo necesario

--- sortfunc.py
+++ sortfunc.py
@@ -26,22 +26,17 @@ def sortBurbuja(Lista):
         que se toma el algoritmo de burbuja
         para ordenar la tupla L
     '''
-    archivo = open('burbuja.txt', 'w')
     c = 0
     L = list(Lista)
     tam = len(L)
     for i in range(1, tam):
         for j in range(0, tam - i):
-            archivo.write('')
             if(L[j] > L[j + 1]):
                 k = L[j + 1]
                 L[j + 1] = L[j]
                 L[j] = k
             c += 1
-
     return c
-
-
 def sortSeleccion(Lista):
     ''' Recibe una lista de floats
         Retorna la cantidad de ciclos (int)
@@ -52,12 +47,11 @@ def sortSeleccion(Lista):
     tam = len(L)
     c = 0
     for i in range(tam - 1):
-        minimo = i
         for j in range(i + 1, tam):
-            if L[minimo] > L[j]:
-                minimo = j
+            if L[i] > L[j]:
+                i = j
             c += 1
-        aux = L[minimo]
-        L[minimo] = L[i]
+        aux = L[i]
+        L[i] = L[i]
         L[i] = aux
     return c
